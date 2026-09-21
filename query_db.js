8e9d6867a4d3917f4043a26220404c63627eb983
abhisrisrivastava01-SW <abhisri.srivastava01@gmail.com>
2026-09-22 04:45:55 +0530

    Delete query_db.js

--- query_db.js
+++
@@ -1,4 +0,0 @@
-const Database = require('C:/Users/user/AppData/Roaming/npm/node_modules/omniroute/node_modules/better-sqlite3');
-const db = new Database('C:/Users/user/.omniroute/storage.sqlite');
-console.log('Tables:');
-console.log(db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all());
